afa5d5fe9867924df12482f7c34b0f26ec683511
Darshana Keerthirathna <keerthi.mac@gmail.com>
2025-01-07 07:49:17 +0530

    commit msg

--- src/main/java/api/Customer.java
+++ src/main/java/api/Customer.java
@@ -9,21 +9,38 @@ import java.io.IOException;
 public class Customer extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Query param
+        //Path Variables
+        //Headers
+        //Body - Not a good practice
         resp.getWriter().println("ok");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        //Query param
+        String id = req.getParameter("id");
+        resp.getWriter().println(id);
+        //Path Variables
+        //Headers
+        String name = req.getHeader("name");
+        resp.getWriter().println(name);
+        //Body
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPut(req, resp);
+        //Query param
+        //Path Variables
+        //Headers
+        //Body
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
+        //Query param
+        //Path Variables
+        //Headers
+        //Body
     }
 }
